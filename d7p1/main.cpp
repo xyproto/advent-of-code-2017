8a1885cb59e98dcdc15e4a37d55fb65d2643fa9d
Alexander F Rødseth <rodseth@gmail.com>
2017-12-14 20:59:36 +0100

    Solve day 7 part 1

--- d7p1/main.cpp
+++ d7p1/main.cpp
@@ -13,20 +13,61 @@ using std::cout;
 //
 //----------------------------
 
-vector<string> others(string line) {
-    auto rightpart = split(line, "->").second;
-    vector<string> words = splitv(rightpart, ", ");
-    return trimv(words);
+const vector<string> others(const string line) {
+    return words(split(line, "->").second, ", ");
 }
 
-string word(string line) {
+const string word(const string line) {
+    return trim(split(line, "->").first);
 }
 
+bool has(vector<string> words, string word) {
+    for (const auto &w: words) {
+        if (w == word) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const vector<string> top_words(vector<string> lines) {
+    vector<string> pointed_to {}; // words that have been pointed too
+    vector<string> pointing {}; // words that are pointing to other words
+    vector<string> all_words {};
+    for (const auto &line: lines) {
+        const auto word = words(line)[0];
+        all_words.push_back(word);
+        const auto other_words = others(line);
+        //cout << word << " => ";
+        if (other_words.size() > 0) {
+            // This word is pointing at other words
+            pointing.push_back(word);
+        }
+        for (const auto &other_word: other_words) {
+            //cout << other_word << "; ";
+            // Add the word to the list of words that has been pointed too
+            pointed_to.push_back(other_word);
+        }
+        //cout << endl;
+    }
+    // Top words are not pointed to, but pointing
+    vector<string> top_words {};
+    for (const auto &word: all_words) {
+        if (has(pointing, word) && !has(pointed_to, word)) {
+            top_words.push_back(word);
+        }
+    }
+    return top_words;
+}
 
 int main() {
-    auto line = "fwft (72) -> ktlj, cntj, xhth"s;
-    for (auto &word: others(line)) {
-        cout << "WORD: " << word << endl;
+    cout << "--- input.txt ---" << endl;
+    for (const auto &tw: top_words(readlines("input.txt"))) {
+        cout << "top word: " << tw << endl;
     }
-    cout << line << endl;
+
+    cout << "--- input2.txt ---" << endl;
+    for (const auto &tw: top_words(readlines("input2.txt"))) {
+        cout << "top word: " << tw << endl;
+    }
 }
