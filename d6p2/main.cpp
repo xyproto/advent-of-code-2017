d5a30e4daec4ba64d879f18d5422f9f941450dc7
Alexander F Rødseth <rodseth@gmail.com>
2017-12-14 10:01:08 +0100

    Add day 6 part 2

---
+++ d6p2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include "test.h"
+#include "util.h"
+#include "memory.h"
+
+using std::cout;
+
+int main() {
+    cout << "ANSWER: " << endl << Memory(vector<int> {2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14}).must_redistribution_number_full() << endl;
+}
